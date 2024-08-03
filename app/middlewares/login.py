f33c8b01e9c8d81cf777d96c12f43e1700b61597
armanheids <armanheids@gmail.com>
2024-08-03 11:06:16 +0330

    middleware added

---
+++ app/middlewares/login.py
@@ -0,0 +1,34 @@
+from functools import wraps
+
+# Database ORMs
+from db import db, User, email_len, password_len
+
+import json
+from datetime import datetime, timedelta
+import jwt
+from flask import request, jsonify, make_response
+from app import app
+from app.jetCert_setup import Login
+
+
+# decorator for verifying the user
+def login(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        # creates dictionary of form auth data
+        auth = request.form
+
+        # returns 400 if any email or / and password is missing
+        if not auth or not auth.get("email") or not auth.get("password"):
+            return make_response("Email or password is missing!", 400)
+
+        email = auth.get("email")
+        password = auth.get("password")
+
+        response = Login.run(inputs={"email": email, "password": password})
+
+        print(response)
+        is_login = response.get("result").get("login")
+        return f(is_login, *args, **kwargs)
+
+    return decorated
