fc8d26b42e243e1a7cb8c9e8441ca4b0622eea2b
armanheids <armanheids@gmail.com>
2024-09-01 01:25:23 +0330

    MAPE design updated

--- modules/__MAPE__/planning.py
+++ modules/__MAPE__/planning.py
@@ -6,6 +6,8 @@ print(
         {
             "login": {"state": "safe" if random.randint(0, 1) else "fast", "level": 1},
             "cryptography": {"state": "safe", "level": random.randint(1, 4)},
+            "finance": {"state": "fast", "level": random.randint(1, 2)},
+            "physics": {"state": "fast", "level": random.randint(1, 4)},
         }
     )
 )
