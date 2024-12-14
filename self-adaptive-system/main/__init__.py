5de5f7c20ff41263c8217d6689cc218ee4eaf03f
armanheids <armanheids@gmail.com>
2024-12-14 04:39:14 +0330

    main updated

--- self-adaptive-system/main/__init__.py
+++ self-adaptive-system/main/__init__.py
@@ -1,26 +1,14 @@
 from dotenv import load_dotenv
 from flask import Flask
 
-# Load .env
 load_dotenv()
 
-# Setup app
 app = Flask("JetCert Example")
 
-# Setup Database
 import main.db
 
-# Setup JetCert Modules
 import main.modules
 
-# Setup Middlewares
-from main.middlewares import login
+import main.middlewares
 
-# Setup Handlers
-from main.handlers import (
-    signup,
-    black_scholes,
-    lennard_jones,
-    get_all_users,
-    delete_user,
-)
+import main.handlers
