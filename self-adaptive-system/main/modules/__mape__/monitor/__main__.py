0575717272dab0e70ef5c3df55377a827bfc79b8
armanheids <armanheids@gmail.com>
2025-02-03 15:26:22 +0330

    monitor updated

--- self-adaptive-system/main/modules/__mape__/monitor/__main__.py
+++ self-adaptive-system/main/modules/__mape__/monitor/__main__.py
@@ -4,10 +4,10 @@ import json
 
 def security_center():
     return {
-        "CPU_usage_percentage": random.randint(0, 100),
-        "RAM_usage_percentage": random.randint(0, 100),
-        "login_fail_percentage": random.randint(0, 100),
-        "network_eavesdropping_percentage": random.randint(0, 100),
+        "CPU_usage": random.randint(0, 100),
+        "RAM_usage": random.randint(0, 100),
+        "login_fail": random.randint(0, 100),
+        "network_sniffing": random.randint(0, 100),
     }
 
 
