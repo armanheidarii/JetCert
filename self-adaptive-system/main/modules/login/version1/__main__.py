02c5a50b4f5dff2dcd15cdadbf380ecedc2c41ef
Arman <80320242+armanheidarii@users.noreply.github.com>
2025-02-03 17:56:21 +0330

    Update __main__.py

--- self-adaptive-system/main/modules/login/version1/__main__.py
+++ self-adaptive-system/main/modules/login/version1/__main__.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 from peewee import SqliteDatabase
 from playhouse.reflection import generate_models, print_model, print_table_sql
 
+#
+
 load_dotenv()
 
 db_path = os.getenv("DB_PATH")
