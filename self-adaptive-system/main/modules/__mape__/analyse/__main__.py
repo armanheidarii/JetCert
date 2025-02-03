498c5234d24b25e615da4a4e0e0bcbde250cb6fa
armanheids <armanheids@gmail.com>
2025-02-03 15:26:35 +0330

    analyze updated

--- self-adaptive-system/main/modules/__mape__/analyse/__main__.py
+++ self-adaptive-system/main/modules/__mape__/analyse/__main__.py
@@ -6,73 +6,73 @@ def get_json_inputs():
 
 
 def login_analyse(monitor_data):
-    login_fail_percentage = monitor_data.get("login_fail_percentage")
-    RAM_usage_percentage = monitor_data.get("RAM_usage_percentage")
+    login_fail = monitor_data.get("login_fail")
+    RAM_usage = monitor_data.get("RAM_usage")
 
     login_analyse_data = []
 
-    if login_fail_percentage <= 100 and RAM_usage_percentage <= 100:
+    if login_fail <= 100 and RAM_usage <= 100:
         login_analyse_data.append("version2")
 
-    if login_fail_percentage <= 40 and RAM_usage_percentage <= 45:
+    if login_fail <= 40 and RAM_usage <= 45:
         login_analyse_data.append("version1")
 
     return login_analyse_data
 
 
 def crypto_analyse(monitor_data):
-    network_eavesdropping_percentage = monitor_data.get(
-        "network_eavesdropping_percentage"
+    network_sniffing = monitor_data.get(
+        "network_sniffing"
     )
 
     crypto_analyse_data = []
 
-    if network_eavesdropping_percentage <= 100:
+    if network_sniffing <= 100:
         crypto_analyse_data.append("version4")
 
-    if network_eavesdropping_percentage <= 80:
+    if network_sniffing <= 80:
         crypto_analyse_data.append("version3")
 
-    if network_eavesdropping_percentage <= 40:
+    if network_sniffing <= 40:
         crypto_analyse_data.append("version2")
 
-    if network_eavesdropping_percentage <= 20:
+    if network_sniffing <= 20:
         crypto_analyse_data.append("version1")
 
     return crypto_analyse_data
 
 
 def finance_analyse(monitor_data):
-    RAM_usage_percentage = monitor_data.get("RAM_usage_percentage")
-    CPU_usage_percentage = monitor_data.get("CPU_usage_percentage")
+    RAM_usage = monitor_data.get("RAM_usage")
+    CPU_usage = monitor_data.get("CPU_usage")
 
     finance_analyse_data = []
 
-    if RAM_usage_percentage <= 100 and CPU_usage_percentage <= 100:
+    if RAM_usage <= 100 and CPU_usage <= 100:
         finance_analyse_data.append("version1")
 
-    if RAM_usage_percentage <= 40 and CPU_usage_percentage <= 50:
+    if RAM_usage <= 40 and CPU_usage <= 50:
         finance_analyse_data.append("version2")
 
     return finance_analyse_data
 
 
 def physics_analyse(monitor_data):
-    RAM_usage_percentage = monitor_data.get("RAM_usage_percentage")
-    CPU_usage_percentage = monitor_data.get("CPU_usage_percentage")
+    RAM_usage = monitor_data.get("RAM_usage")
+    CPU_usage = monitor_data.get("CPU_usage")
 
     physics_analyse_data = []
 
-    if RAM_usage_percentage <= 100 and CPU_usage_percentage <= 100:
+    if RAM_usage <= 100 and CPU_usage <= 100:
         physics_analyse_data.append("version1")
 
-    if RAM_usage_percentage <= 80 and CPU_usage_percentage <= 90:
+    if RAM_usage <= 80 and CPU_usage <= 90:
         physics_analyse_data.append("version2")
 
-    if RAM_usage_percentage <= 60 and CPU_usage_percentage <= 70:
+    if RAM_usage <= 60 and CPU_usage <= 70:
         physics_analyse_data.append("version3")
 
-    if RAM_usage_percentage <= 20 and CPU_usage_percentage <= 30:
+    if RAM_usage <= 20 and CPU_usage <= 30:
         physics_analyse_data.append("version4")
 
     return physics_analyse_data
