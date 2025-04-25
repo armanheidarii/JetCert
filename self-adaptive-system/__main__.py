99f4ce7714dd88777dddd2bd12dade11bb166c1a
armanheids <armanheids@gmail.com>
2025-04-25 19:22:18 +0330

    __main__.py updated

--- self-adaptive-system/__main__.py
+++ self-adaptive-system/__main__.py
@@ -1,10 +1,17 @@
 import os
 import sys
+from waitress import serve
 
-from main import app
+from main import logger, app
 
 if __name__ == "__main__":
-    app.run(
-        host=os.getenv("HOST"),
-        port=os.getenv("PORT"),
-    )
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+
+    try:
+        serve(app, host=host, port=int(port))
+    except Exception as e:
+        logger.error(f"Server error: {e}")
+        sys.exit(1)
+    finally:
+        logger.info("Server has been shut down.")
